ed2efe55ff6697848a7821b0b1433778ed873229
CodingLife22 <90231663+CodingLife22@users.noreply.github.com>
2021-09-07 19:29:18 +0530

    Improved code
    checking cell value is now working

--- Main.py
+++ Main.py
@@ -1,6 +1,31 @@
 import openpyxl
+from Dictionary import K
 
-Wb = openpyxl.load_workbook("2020-06-14 - STOCK UPDATE.xlsx")
-print(Wb.active.title)
-Users_working_sheet = input("Enter sheet Name\n")
-Changed_Users_working_sheet = Wb[Users_working_sheet]
+"""
+Important comment
+yellow = 2-5
+green  =>5
+red    =0-2
+
+Red color code "FF240A"
+Green color code "7EFF1F"
+"""
+SHEETNAME = "2020-06-14 - STOCK UPDATE.xlsx"
+Wb = openpyxl.load_workbook(SHEETNAME)
+print(Wb.sheetnames)
+Need = input(
+    """Enter: "packing"for Packing procisure or Enter "Value" to know value: """
+)
+
+
+def Value():
+    pass
+
+
+def packing():
+    pass
+
+
+# Users_working_sheet = input("Enter sheet Name\n")
+# Sheet = Wb[Users_working_sheet]
+# print(Sheet["C8"].value)
